2d09252fdb0c57652592a32bece71e3d540243f0
Jonathan Veit <jonathan.veit@gmail.com>
2015-02-27 19:30:30 -0500

    Update config.php
    Remove password

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'jonveitc_simplewall');
 define('DB_USER', 'jonveitc_simple');
-define('DB_PASS', 'asd90123');
+define('DB_PASS', '');
 	
 ?>
